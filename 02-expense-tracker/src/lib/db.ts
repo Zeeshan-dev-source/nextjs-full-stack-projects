5a8eab556869bbafe7e4724847e75a61f8fbdaa8
zeeshan-deve <zeeshanamd256@gmail.com>
2026-09-19 17:50:19 +0500

    Fix TiDB SSL config

--- 02-expense-tracker/src/lib/db.ts
+++ 02-expense-tracker/src/lib/db.ts
@@ -1,12 +1,15 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  ssl: { rejectUnauthorized: true },
+  host: process.env.DB_HOST,  
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 });
 
 export default pool;
