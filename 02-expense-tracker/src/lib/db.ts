e57282d104a8c83aee6f38de5c35785c5feb5635
zeeshan-deve <zeeshanamd256@gmail.com>
2026-09-21 01:05:35 +0500

    Update database configuration

--- 02-expense-tracker/src/lib/db.ts
+++ 02-expense-tracker/src/lib/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  // ssl: {
-  //   minVersion: "TLSv1.2",
-  //   rejectUnauthorized: true,
-  // },
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
